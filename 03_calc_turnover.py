54113d7263ef5a433778e6234790a3e886f92b35
Leery Corsair <vladleonov21032002@gmail.com>
2025-06-30 14:59:55 +0300

    feat:calc for all

--- 03_calc_turnover.py
+++ 03_calc_turnover.py
@@ -1,14 +1,15 @@
 from typing import Tuple
 from config import logger
 import pandas as pd
+from tqdm import tqdm
 
-TARGET_NM = 391624100
 DAILY_REPORTS_FILE = "data/02_02_daily_reports_filtered.xlsx"
 STOCK_HISTORY_FILE = "data/03_stock_history.xlsx"
 STOCK_HISTORY_SHEET = "Остатки по дням"
+RESULTS_FILE = "data/03_turnover_all.xlsx"
 
 
-def process_stock_history(target_nm: int) -> int:
+def process_stock_history(target_nm: int) -> Tuple[int, bool]:
     df = pd.read_excel(STOCK_HISTORY_FILE, STOCK_HISTORY_SHEET, header=1)
 
     filtered_df = df[df["Артикул WB"] == target_nm]
@@ -24,40 +25,55 @@ def process_stock_history(target_nm: int) -> int:
     stocks_cols = [col for col in filtered_df.columns if col not in exclude_cols]
 
     stocks_sum = 0
+    is_found = False
     if not filtered_df.empty:
         stocks_sum = (
             filtered_df[stocks_cols].select_dtypes(include="number").sum(axis=1).iloc[0]
         )
-        logger.info(f"Total stocks for nm = {target_nm}: {stocks_sum}")
-    else:
-        logger.info(f"No data for nm = {target_nm}")
+        is_found = True
 
-    return stocks_sum
+    return stocks_sum, is_found
 
 
-def process_daily_reports(target_nm: int) -> Tuple[int, int]:
-    df = pd.read_excel(DAILY_REPORTS_FILE)
-
+def process_daily_reports(df: pd.DataFrame, target_nm: int) -> Tuple[int, int]:
     filtered_df = df[df["Код номенклатуры"] == target_nm]
     counts = filtered_df["Тип документа"].value_counts()
     sales, refunds = counts.get("Продажа", 0), counts.get("Возврат", 0)
 
-    logger.info(f"Total sales, refunds for nm = {target_nm}: {sales}, {refunds}")
-
     return sales, refunds
 
 
-def run_calc_turnover() -> None:
-    stocks = process_stock_history(TARGET_NM)
-    sales, refunds = process_daily_reports(TARGET_NM)
+def run_calc_turnover_all() -> None:
+    df = pd.read_excel(DAILY_REPORTS_FILE)
+    unique_nms = df["Код номенклатуры"].dropna().unique()
 
-    turnover = stocks / (sales - refunds)
-    logger.info(f"Turnover for nm = {TARGET_NM}: {turnover}")
+    results = []
+    for target_nm in tqdm(unique_nms, "Processing NMs"):
+        try:
+            stocks, is_found = process_stock_history(target_nm)
+            sales, refunds = process_daily_reports(df, target_nm)
+            turnover = stocks / (sales - refunds) if (sales - refunds) != 0 else None
+
+            results.append(
+                {
+                    "nm_id": target_nm,
+                    "is_found": is_found,
+                    "stocks": stocks,
+                    "sales": sales,
+                    "refunds": refunds,
+                    "turnover": turnover,
+                }
+            )
+        except Exception as e:
+            logger.error(f"Error processing nm = {target_nm}: {e}")
+
+    results_df = pd.DataFrame(results)
+    results_df.to_excel(RESULTS_FILE, index=False)
 
 
 def main() -> None:
     logger.info("Starting calc turnover script...")
-    run_calc_turnover()
+    run_calc_turnover_all()
 
 
 if __name__ == "__main__":
